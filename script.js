71f8325ba044faa6e79ed3ceb0dcb1a1c0e79596
Jhone Dourado <72326178+jhonedourado@users.noreply.github.com>
2026-08-28 18:55:05 -0300

    Update linkDrive script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
                 document.title = `Guia em Áudio - ${informacoesAudio.titulo}`;
 
                 // Monta link do Drive usando o ID
-                https://drive.google.com/uc?export=open&id=${informacoesAudio.audio_id}`;
+                const linkDrive = `https://drive.google.com/uc?export=open&id=${informacoesAudio.audio_id}`;
                 fonte.src = linkDrive;
                 audio.load();
 
